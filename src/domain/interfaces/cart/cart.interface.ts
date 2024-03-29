8fd2ad3af6716ca565b0ac20053c918d6a600d7a
Zarak Khattak <134920351+zarakkhattak96@users.noreply.github.com>
2024-03-30 02:01:20 +0500

    add cart base interface

---
+++ src/domain/interfaces/cart/cart.interface.ts
@@ -0,0 +1,22 @@
+export interface CartInterface {
+  id?: number;
+  price: number | null;
+  quantity: number | null;
+  shippingFee: number | null;
+  total: number | null;
+  description: string | null;
+  productId: number;
+  userId: number;
+  productName: string | null;
+}
+
+export interface CartBaseInterface {
+  addToCart(cart: CartInterface): Promise<CartInterface>;
+  fetchAllFromCart(): Promise<CartInterface[]>;
+  fetchOneFromCart(id: number): Promise<CartInterface> | null;
+  deleteFromCart(id: number): Promise<CartInterface>;
+  updateInCart(
+    prodInCartId: number,
+    cart: CartInterface,
+  ): Promise<CartInterface>;
+}
