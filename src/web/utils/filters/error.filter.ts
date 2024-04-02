decd8903b8eb42220c3ce701ff33fa2698183634
Zarak Khattak <134920351+zarakkhattak96@users.noreply.github.com>
2024-04-02 17:03:19 +0500

    comment out error.filters.ts file

--- src/web/utils/filters/error.filter.ts
+++ src/web/utils/filters/error.filter.ts
@@ -1,18 +1,18 @@
-import { AppError } from '@app/app.errors';
-import { Response } from 'express';
-
-const CODE_TO_STATUS = new Map<string, number>([
-  ['not_found', 404],
-  ['invalid_request', 400],
-  ['bad_request', 400],
-]);
-
-const handleError = (err: AppError, resp: Response) => {
-  const status = CODE_TO_STATUS.get(err.code) || 400;
-
-  const extra = err.extra || {};
-
-  const body = { code: err.code, description: err.description, ...extra };
-
-  return resp.status(status).send(body);
-};
+// import { AppError } from '@app/app.errors';
+// import { Response } from 'express';
+//
+// const CODE_TO_STATUS = new Map<string, number>([
+//   ['not_found', 404],
+//   ['invalid_request', 400],
+//   ['bad_request', 400],
+// ]);
+//
+// const handleError = (err: AppError, resp: Response) => {
+//   const status = CODE_TO_STATUS.get(err.code) || 400;
+//
+//   const extra = err.extra || {};
+//
+//   const body = { code: err.code, description: err.description, ...extra };
+//
+//   return resp.status(status).send(body);
+// };
